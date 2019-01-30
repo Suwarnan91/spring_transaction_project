38a28e357c8d671a72eb38027d96742686b6f80b
Suwarnan91 <Suwarnan91@gmail.com>
2019-01-30 18:31:06 +0530

    bank system transaction

---
+++ bankingSystem/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+Insert into bank_account.Bank_Account(ID, Full_Name, Balance) values (1, 'Tom', 1000);
+Insert into bank_account.Bank_Account(ID, Full_Name, Balance) values (2, 'Jerry', 2000);
+Insert into bank_account.Bank_Account(ID, Full_Name, Balance) values (3, 'Donald', 3000);
+
